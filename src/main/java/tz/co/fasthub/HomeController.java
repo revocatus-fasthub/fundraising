215f5650418d82b0122104446a9a3405b82de09c
revocatus-fasthub <revocatus.robert@fasthub.co.tz>
2017-09-12 17:46:34 +0300

    first commit

---
+++ src/main/java/tz/co/fasthub/HomeController.java
@@ -0,0 +1,18 @@
+package tz.co.fasthub;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Boniface Chacha on 07-Dec-16.
+ */
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+
+    @RequestMapping("/index")
+    public String home(){
+        return "home";
+    }
+}
